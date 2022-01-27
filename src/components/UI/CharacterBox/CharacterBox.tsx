bc7a82c757ea518fedf3e60738620cbce60a120a
Nick Leslie <nleslie@jadeworld.com>
2022-01-28 12:55:36 +1300

    Minor css animations added

--- src/components/UI/CharacterBox/CharacterBox.tsx
+++ src/components/UI/CharacterBox/CharacterBox.tsx
@@ -5,11 +5,11 @@ const CharacterBox = (props: CharacterBoxProps) => {
   return (
     <div
       className={`character-box 
-      ${props.usedLocationIncorrect && "incorrect-location"} 
-        ${props.usedLocationCorrect && "correct-location"} 
-        ${props.notUsed && "not-used"}
-        ${props.invalid && "invalid"}
-        `}
+        ${props.usedLocationIncorrect && "incorrect-location"} 
+          ${props.usedLocationCorrect && "correct-location"} 
+          ${props.notUsed && "not-used"}
+          ${props.invalid && "invalid"}
+          `}
     >
       <p className="character-box__text">{props.character}</p>
     </div>
