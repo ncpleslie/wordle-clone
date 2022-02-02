258265b9f1c877dec3534274b6c10336ed30cc3b
Nick Leslie <nleslie@jadeworld.com>
2022-02-02 18:36:06 +1300

    Removed some text for better mobile experience

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,10 @@ import GithubIcon from "../UI/Icon/GithubIcon";
 
 const Header = () => {
   return (
-    <nav className="border w-full text-center p-2 shadow grid grid-cols-3 justify-center">
+    <nav className="border w-full text-center p-2 shadow grid grid-cols-3 justify-center items-center">
       <h1 className="font-bold col-start-2">Another Wordle Clone</h1>
-      <a
-        href={AppConstants.sourceCodeUrl}
-        className="flex flex-row gap-1 items-center ml-auto"
-      >
+      <a href={AppConstants.sourceCodeUrl} className=" ml-auto">
         <GithubIcon />
-        Source Code
       </a>
     </nav>
   );
