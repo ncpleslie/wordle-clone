1100b84359e94a180e7fb4372d65b8521e5f4fc1
ncpleslie <33884283+ncpleslie@users.noreply.github.com>
2022-02-04 04:30:43 +0000

    Deploying to gh-pages from @ ncpleslie/wordle-clone@a01ba3b3997017b40c3aecfa64c5ac0e42690995 🚀

--- assets/config.639d2aae.js
+++
@@ -1,1 +0,0 @@
-const n=5,t=6,e="en-US";var o={wordLength:n,tries:t,lang:e};export{o as default,e as lang,t as tries,n as wordLength};
---
+++ assets/config.81b6a2f7.js
@@ -0,0 +1,1 @@
+const t=5,s=6,e=["en-US"];var n={wordLength:t,tries:s,supportedLangs:e};export{n as default,e as supportedLangs,s as tries,t as wordLength};
